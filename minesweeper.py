f5c204b7b621639de9ebbd2eec09e8e78bc15e1a
Mocoma <mocomaminecraft@gmail.com>
2018-09-28 22:57:24 +0200

    fixed a couple of bugs expanded the grid again so its kind of nice

--- minesweeper.py
+++ minesweeper.py
@@ -1,7 +1,7 @@
 from mocoma_minesweeper_src import ConsoleIO, MinesweeperGrid
 
 io = ConsoleIO()
-grid = MinesweeperGrid.gen_random(3, 3, 1)
+grid = MinesweeperGrid.gen_random(10, 10, 10)
 while not grid.ended():
     io.show_grid(grid)
     action, coords = io.get_grid_input(grid.width, grid.height)
@@ -10,5 +10,5 @@ while not grid.ended():
     elif action == "show":
         grid.show_empty_cells(*coords)
 
-io.print_end(won=grid.is_won())
-
+io.show_grid(grid)
+io.print_end(won=grid.is_win())
